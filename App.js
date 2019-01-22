f9c4a503efb3ea0869d75c151fa408bea5872c51
Judith Prays <judithprays@gmail.com>
2019-01-22 05:32:37 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet  } from 'react-native';
+
+access_token = '54439341.688ae00.74302597d11742d8aa26077448a8fc68'
+
+state = {
+    loaded: false,
+    data: null,
+    comments: [],
+}
+
+class GeolocationExample extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    this.watchId = navigator.geolocation.watchPosition(
+      (position) => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null,
+        });
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    );
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  render() {
+    return (
+      <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
+        {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+      </View>
+    );
+  }
+}
+
+export default GeolocationExample;
