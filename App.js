8b23ae852b39293cb74ebe986861f3e8819cff27
Judith Prays <judithprays@gmail.com>
2019-02-13 11:45:15 -0800

    testing moment

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Fetch } from 'react-data-fetching'
 import { View, Text, StyleSheet, Alert } from 'react-native';
+import { Moment } from 'react-moment';
+import 'moment-timezone';
 
 // access_token = '54439341.688ae00.74302597d11742d8aa26077448a8fc68'
 
@@ -10,6 +12,9 @@ state = {
     comments: [],
 }
 
+import moment from 'moment';
+var now = moment().format();
+
 class GeolocationExample extends Component {
   constructor(props) {
     super(props);
@@ -17,22 +22,40 @@ class GeolocationExample extends Component {
     this.state = {
       latitude: null,
       longitude: null,
+      data: null,
       error: null, 
      };
   }
 
   componentDidMount() {
-    this.watchId = navigator.geolocation.watchPosition(
-      (position) => {
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null,
-        });
+    fetch('https://contact.plaid.com/jobs', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
       },
-      (error) => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
-    );
+      body: JSON.stringify({
+        "name": "Judith Prays",
+        "email": "judithprays@gmail.com",
+        "resume": "https://www.linkedin.com/in/judith-prays-1b361631/",
+        "github": "github.com/judithprays", 
+        "twitter": "@judithprays", 
+        "website": "http://www.duhstudio.com/judith.html" }),
+    });
+
+
+
+    // this.watchId = navigator.geolocation.watchPosition(
+    //   (position) => {
+    //     this.setState({
+    //       latitude: position.coords.latitude,
+    //       longitude: position.coords.longitude,
+    //       error: null,
+    //     });
+    //   },
+    //   (error) => this.setState({ error: error.message }),
+    //   { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, distanceFilter: 10 },
+    // );
   }
 
   componentWillUnmount() {
@@ -41,15 +64,24 @@ class GeolocationExample extends Component {
 
   render() {
 
-    var SampleNameArray = [ "Killin", "it", "😍"];
+    var SampleNameArray = [ "Killin", "it"];
+
+    // Get Time Difference between local time and UTC
+    var timedifference = new Date().getTimezoneOffset();
+
+    // Convert to hours
+    timedifference = timedifference/60;
+
 
     return (
       <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Fetch
-              url="https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400"
+              url="https://api.sunrise-sunset.org/json?lat=34.0521145&lng=-118.3811206"
             >
-              {({ data }) => (<Text>{data.results.sunrise}</Text>)}
+              {({ data }) => (<Text>{data.results.sunset}</Text>)}
             </Fetch>
+            <Text>{now}</Text>
+            <Text>{timedifference}</Text>
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
         {SampleNameArray.map((item, key)=>(
