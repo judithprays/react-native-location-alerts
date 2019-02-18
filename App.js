4a8611fc0714161145670364b83c367d0495386c
judith prays <judithprays@gmail.com>
2019-02-18 14:21:27 -0800

    lat long adjust

--- App.js
+++ App.js
@@ -43,6 +43,8 @@ class GeolocationExample extends Component {
       <View style={{ flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Latitude: {this.state.latitude}</Text>
         <Text>Longitude: {this.state.longitude}</Text>
+        <Text>Latitude: {this.state.latitude}</Text>
+        <Text>Longitude: {this.state.longitude}</Text>
         {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
       </View>
     );
